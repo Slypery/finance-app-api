68d97219c9e25c0ac4c3645551eec4d3001f873d
Slypery <ibnuumar634@gmail.com>
2026-09-21 16:27:19 +0700

    change "user" module to "me" module

--- src/modules/me/me.route.ts
+++ src/modules/me/me.route.ts
@@ -3,10 +3,10 @@ import { db } from '@/db/index.js'
 import { authMiddleware, type AuthEnv } from '@/middleware/auth.middleware.js'
 import { Hono } from 'hono'
 
-export const userRoute = new Hono<AuthEnv>()
-userRoute.use(authMiddleware)
+export const meRoute = new Hono<AuthEnv>()
+meRoute.use(authMiddleware)
 
-userRoute.get('/profile', async (c) => {
+meRoute.get('/profile', async (c) => {
   const userId = c.get('userId')
 
   const user = await db.query.users.findFirst({
--- src/modules/routes.ts
+++ src/modules/routes.ts
@@ -1,10 +1,10 @@
 import { authRoute } from '@/modules/auth/auth.route.js'
-import { userRoute } from '@/modules/user/user.route.js'
+import { meRoute } from '@/modules/me/me.route.js'
 import { workspaceRoute } from '@/modules/workspace/workspace.route.js'
 import { Hono } from 'hono'
 
 export const appRoute = new Hono()
 
 appRoute.route('/auth', authRoute)
-appRoute.route('/user', userRoute)
-appRoute.route('/workspace/:workspaceId', workspaceRoute)
+appRoute.route('/me', meRoute)
+appRoute.route('/workspaces/:workspaceId', workspaceRoute)
